a597cf30040e6deac284567d5ae56c4de2848e53
Bob Habig <rfhab2@gmail.com>
2018-03-15 19:51:11 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  
+  let index = 0;
+  
+  let body = document.querySelector('body');
+  body.addEventListener('keyDown', checkCode);
+  
+  function checkCode(e){
+    const key = parseInt(e.detail || e.which);
+    
+    if (key === code[i]) {
+      index++;
+      
+      if (index === code.length) {
+        alert("code compelte");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
 }
